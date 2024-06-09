1924a723aa4694b565ea5f16dbe827b4342e6724
Sameer Bakshi <sameerbakshi34@gmail.com>
2024-06-09 23:05:36 +0530

    Utils code

--- src/db/index.js
+++ src/db/index.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
     console.log(
-      `\n MongoDB COnnected!! DB HOST: ${connectionInstance.connection.host}`
+      `\n MongoDB Connected!! DB HOST: ${connectionInstance.connection.host}`
     );
   } catch (error) {
     console.log("MONGODB connection FAILED: ", error);
