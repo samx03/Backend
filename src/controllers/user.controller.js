d0fdd27063b91e37a589ae96185bc63ed0a02a59
Sameer Bakshi <sameerbakshi34@gmail.com>
2024-06-12 16:57:49 +0530

    Fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,7 +7,6 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 const registerUser = asyncHandler(async (req, res) => {
   // Get user details from frontend
   const { fullName, email, username, password } = req.body;
-  console.log(fullName, email);
 
   // Validation - not empty
   if (
@@ -22,12 +21,20 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 
   if (existedUser) {
-    throw new ApiError(409, "User with email of username already exists");
+    throw new ApiError(409, "User with email or username already exists");
   }
 
   // Check for images, check for avatars
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar is required");
