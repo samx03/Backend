59abaa87381c27ff5b2f82933bf741b44e08f7f6
Sameer Bakshi <sameerbakshi34@gmail.com>
2024-06-25 13:19:03 +0530

    Tweet controller and routes

--- src/app.js
+++ src/app.js
@@ -20,9 +20,11 @@ app.use(cookieParser());
 //Can import with any name since i have used export default in the router file
 import userRouter from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
 
 //routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/tweets", tweetRouter);
 
 export { app };
---
+++ src/controllers/tweet.controller.js
@@ -0,0 +1,121 @@
+import { Tweet } from "../models/tweet.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { isValidObjectId } from "mongoose";
+
+const createTweet = asyncHandler(async (req, res) => {
+  const { content } = req.body;
+
+  if (!content) {
+    throw new ApiError(404, "Tweet content is required");
+  }
+
+  const createdTweet = await Tweet.create({
+    content,
+    owner: req.user?._id,
+  });
+
+  if (!createdTweet) {
+    throw new ApiError(404, "Something went wrong while creating tweet");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, createdTweet, "Tweet successfully created"));
+});
+
+const getUserTweets = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
+
+  if (!userId) {
+    throw new ApiError(404, "User id is required");
+  }
+
+  const fetchedTweets = await Tweet.find({ owner: userId });
+
+  if (!fetchedTweets) {
+    throw new ApiError(404, "Something went wrong while fetching user tweets");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, fetchedTweets, "User tweets fetched successfully")
+    );
+});
+
+const updateTweet = asyncHandler(async (req, res) => {
+  const { content } = req.body;
+  const { tweetId } = req.params;
+
+  if (!content) {
+    throw new ApiError(404, "Content cannot be empty");
+  }
+
+  if (!isValidObjectId(tweetId)) {
+    throw new ApiError(404, "Invalid object id");
+  }
+
+  const tweet = await Tweet.findById(tweetId);
+  if (!tweet) {
+    throw new ApiError(404, "Tweet not found");
+  }
+
+  if (tweet.owner._id.toString() !== req.user?._id.toString()) {
+    throw new ApiError(
+      400,
+      "You cannot update this tweet since you are not the owner"
+    );
+  }
+
+  const updatedTweet = await Tweet.findByIdAndUpdate(
+    tweetId,
+    {
+      $set: {
+        content,
+      },
+    },
+    { new: true }
+  );
+
+  if (!updatedTweet) {
+    throw new ApiError(400, "Some error occured while updating tweet");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updatedTweet, "Tweet updated successfully"));
+});
+
+const deleteTweet = asyncHandler(async (req, res) => {
+  const { tweetId } = req.params;
+
+  if (!isValidObjectId(tweetId)) {
+    throw new ApiError(400, "Invalid tweet id");
+  }
+
+  const tweet = await Tweet.findById(tweetId);
+  if (!tweet) {
+    throw new ApiError(404, "Tweet not found");
+  }
+
+  if (tweet.owner._id.toString() !== req.user?._id.toString()) {
+    throw new ApiError(
+      400,
+      "You cannot delete this tweet since you are not the owner."
+    );
+  }
+
+  const deletedTweet = await Tweet.findByIdAndDelete(tweetId);
+
+  if (!deletedTweet) {
+    throw new ApiError(400, "Something went wrong while deleting this tweet");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, deletedTweet, "Tweet deleted successfully."));
+});
+
+export { createTweet, getUserTweets, updateTweet, deleteTweet };
