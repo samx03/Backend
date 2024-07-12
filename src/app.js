cc49ed95240c85484067766087f6a259a2fee913
Sameer Bakshi <sameerbakshi34@gmail.com>
2024-07-12 16:54:33 +0530

    Subscription controller

--- src/app.js
+++ src/app.js
@@ -24,6 +24,7 @@ import tweetRouter from "./routes/tweet.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
 import likesRouter from "./routes/likes.routes.js";
 import commentRouter from "./routes/comment.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 
 //routes declaration
 app.use("/api/v1/users", userRouter);
@@ -32,5 +33,6 @@ app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/playlist", playlistRouter);
 app.use("/api/v1/likes", likesRouter);
 app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 export { app };
